bfc1d68244f369b13abb496c87164f1a76c88687
Daniel Zeiberg <d.zeiberg@northeastern.edu>
2025-09-05 17:36:50 -0400

    test script works

--- explorer_test.sh
+++ explorer_test.sh
@@ -7,12 +7,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8G
-#SBATCH --array=0-20%4
+#SBATCH --array=0-4%4
 
 SCORESETS_DIR=/projects/talisman/dzeiberg/pillar_project_data/dataset_09042025/scoresets
 RESULTS_DIR=/projects/talisman/dzeiberg/calibration_results/$(date +%Y%m%d_%H%M%S)
 KWARGS_FILE=/home/d.zeiberg/calibration_summary_utils/test_kwargs.json
 module load anaconda3/2024.06
+conda init
+source /home/d.zeiberg/.bashrc
 conda activate assay_calibration
 
 python /home/d.zeiberg/calibration_summary_utils/run.py \
